fe4d43ced07ebc6259ca81348d617c226972079e
SaniyaFarooqui <saniyafarooqui05@gmail.com>
2023-12-04 21:01:05 +0530

    typescript funcion

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,48 @@
-let saniya :string = "Saniya"
+let saniya : string = "Saniya"
 console.log(saniya)
 
 let sufiya :number = 12
 console.log(sufiya)
 
-let saniyavalue : Array<string> = ["s","a","n","i","y","a"]
-let saniynumber : Array<number> = [1,2,3,4,5,6,7,8,9,10]
+let alfiyadata :boolean = true
+  let saniyavalue : Array<string> = ["s","a","n","i","y","a"]
+console.log(saniyavalue)
+let saniyanumber : Array<number> = [1,2,3,4,5,6,7,8,9,10]
+console.log(saniyanumber)
+
+interface fullname{
+    name:string,
+    surname:string,
+    age:number
+}
+
+let objectvalue : fullname = {
+    name:"arbaz",
+    surname:"Khan",
+    age:21
+}
+
+function SaniyaMethod(v1 : string | string[] | number ,v2:string) : string{
+    return v1+" "+v2
+}
+let valueDatatype = SaniyaMethod("Arbaz","Khan");
+console.log(valueDatatype)
+
+//typescript for arrow function
+let SaniyaMethod2 = (value1:number,value2:number) : number => {
+    return value1+value2
+}
+let data = SaniyaMethod2(12,12);
+console.log(data)
+
+//optional ( ? )
+interface Saniya{
+    name : string,
+    surname : string,
+    age ? : number
+}
+
+let arbaz :Saniya = {
+    name:"arbaz",
+    surname:"Khan",
+} 
